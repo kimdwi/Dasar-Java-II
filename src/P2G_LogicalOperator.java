5534ba0ea183122946915a0aa551bce76ffcfcdd
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 19:50:25 +0700

    Menggunakan operator logika

---
+++ src/P2G_LogicalOperator.java
@@ -0,0 +1,17 @@
+public class P2G_LogicalOperator {
+    public static void main(String[] args) {
+        //Menginisialisasikan Variabel
+        int x = 7, y = 11, z = 11;
+        //Menampilkan Variabel
+        System.out.println("X = " +x);
+        System.out.println("Y = " +y);
+        System.out.println("Z = " +z);
+        //Melakukan Operasi Logika
+        System.out.println("X < Y = " +(x<y));
+        System.out.println("X > Y = " +(x>y));
+        System.out.println("Y <= Z = " +(y<=z));
+        System.out.println("X >= Y = " +(x>=y));
+        System.out.println("Y == Z = " +(y==z));
+        System.out.println("X != z = " +(x!=z));
+    }
+}
