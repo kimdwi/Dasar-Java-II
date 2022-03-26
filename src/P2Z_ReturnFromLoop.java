1464aab95676af5850a9cda56db3542d2cc0191f
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 20:25:48 +0700

    Menggunkan return pada perulangan for

---
+++ src/P2Z_ReturnFromLoop.java
@@ -0,0 +1,24 @@
+public class P2Z_ReturnFromLoop {
+    public static void main(String[] args) {
+        returnLoop();
+    }
+    //Membuat Class returnLoop()
+    static void returnLoop() {
+        int x;
+        System.out.println("Example Of Return In For-Loop");
+        //Perulangan For
+        for (x=1; x<=10; x++) {
+            //Percabangan If Else
+            if (x<5) {
+                System.out.printf("%d" ,(x-1));
+            }
+            else {
+                System.out.print("\n");
+                return;
+            }
+            System.out.printf("%d" ,x);
+        }
+        System.out.print("Tidak Akan Pernah Dieksekusi");
+
+    }
+}
