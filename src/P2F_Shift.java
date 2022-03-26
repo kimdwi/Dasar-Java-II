88ddcf73faa75f5ccd7c44e643880604a5be1283
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 19:48:46 +0700

    Melakukan operasi shift

---
+++ src/P2F_Shift.java
@@ -0,0 +1,12 @@
+public class P2F_Shift {
+    public static void main(String[] args) {
+        //Menginisialisasikan Variabel
+        int x = 7;
+        //Menampilkan Variabel
+        System.out.println("X = " +x);
+        //Melakukan Operasi Shift
+        System.out.println("X >> 2 = " +(x>>2));
+        System.out.println("x << 1 = " +(x<<1));
+        System.out.println("X >>> 1 = " +(x>>>1));
+    }
+}
