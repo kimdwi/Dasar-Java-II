265f74f3b428d61e2caa74d5a272b4176fc6a08a
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 19:45:31 +0700

    Melakukan operasi bit

---
+++ src/P2D_Bitwise.java
@@ -0,0 +1,12 @@
+public class P2D_Bitwise {
+    public static void main(String[] args) {
+        //Mendeklarasikan Variabel
+        int x = 5, y = 6;
+        //Menampilkan Variabel
+        System.out.println("X = " +x);
+        System.out.println("Y = " +y);
+        System.out.println("X & Y = " +(x&y));
+        System.out.println("X | Y = " +(x|y));
+        System.out.println("X ^ Y + " +(x^y));
+    }
+}
