9230dd04a3cf273647b9400b07f210657be5d51f
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 19:38:42 +0700

    Mengamati hasil perubahan nilai dari suatu operasi matematis

---
+++ src/P2A_OperatorA.java
@@ -0,0 +1,14 @@
+public class P2A_OperatorA {
+    public static void main(String[] args) {
+        //Mendeklarasikan Dan Menginisialiasikan Variabel
+        float number1 = 15;
+        float number2 = 6;
+        //Melakukan Operasi Matematis
+        float result = number1 / number2;
+        float remainder = number1 % number2;
+        //Menampilkan Variabel
+        System.out.println(number1 + " Divided By " +number2);
+        System.out.println("\nResult \tRemainder");
+        System.out.println(result+ "\t" +remainder);
+    }
+}
