58aeb6dccd04d730ab18e6caf433d234bef8dc62
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 19:44:08 +0700

    Melakukan increment dan decrement nilai

---
+++ src/P2C_InDec.java
@@ -0,0 +1,13 @@
+public class P2C_InDec {
+    public static void main(String[] args) {
+        //Mendeklarasikan Dan Menginisialisasikan Variabel
+        int x = 8, y = 13;
+        //Menampilkan Variabel
+        System.out.println("X = " +x);
+        System.out.println("Y = " +y);
+        System.out.println("X = " + ++x);
+        System.out.println("Y = " + y++);
+        System.out.println("X = " + x--);
+        System.out.println("Y = " + --y);
+    }
+}
