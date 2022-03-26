d0aac447d1178d18e179759d7e41b69177e460ee
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 19:47:24 +0700

    Melakukan operasi komplemen

---
+++ src/P2E_BitwiseComplement.java
@@ -0,0 +1,11 @@
+public class P2E_BitwiseComplement {
+    public static void main(String[] args) {
+        //Mendeklarasikan & Menginisialisasikan Variabel
+        int x = 8;
+        //Menampilkan Variabel
+        System.out.println("X = " +x);
+        //Melakukan Operasi Komplemen
+        int y = ~x;
+        System.out.println("Y = " +y);
+    }
+}
