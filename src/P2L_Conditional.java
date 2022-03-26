756b3cc377a25cf360fbabb76d604ac0dc225622
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 19:58:39 +0700

    Menggunakan operator kondisi

---
+++ src/P2L_Conditional.java
@@ -0,0 +1,12 @@
+public class P2L_Conditional {
+    public static void main(String[] args) {
+        //Mendeklarasikan & Menginisialisasikan Variabel
+        int x = 0;
+        boolean isEven = false;
+        //Menampilkan Variabel
+        System.out.println("X = " +x);
+        //Melakukan Perbandingan Menggunakan Operator Kondisi
+        x = isEven ? 4:7;
+        System.out.println("X = " +x);
+    }
+}
